78cffa9cc7817b060af849e365934e1850578cb9
adrianmdevs <murigucharles3@gmail.com>
2024-02-26 15:47:16 +0300

    First commit

---
+++ resources/views/landingPage.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @vite('resources/css/app.css')
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>laravel tailwind</title>
+</head>
+<body>
+    <h1 class="text-3xl font-bold underline">This is the landing Page</h1>
+</body>
+</html>
---
+++ resources/views/services.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @vite('resources/css/app.css')
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Services</title>
+</head>
+<body>
+    <h1 class="font-light text-center">This is the services section</h1>
+</body>
+</html>
